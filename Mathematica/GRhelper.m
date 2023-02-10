0620c5af3d40c97bab8b3bc5b30c24a0985f0ccf
Bob Seaton <robert.w.seaton.jr@gmail.com>
2023-02-10 16:38:21 -0500

    debugged index ordering

--- Mathematica/GRhelper.m
+++ Mathematica/GRhelper.m
@@ -190,11 +190,9 @@ Begin["`Private`"]
 InverseMetric[g_] := Block[{res}, res =Inverse[g]; Simplify[res]]
 
 ChristoffelSymbols[metric_, coord_,
-   OptionsPattern[{
-    FontSize -> 16, 
-    FontFamily -> "American Typewriter", 
-    Header -> True, 
-    RemoveZeros -> True}]] := 
+   OptionsPattern[{FontSize -> 16, 
+     FontFamily -> "American Typewriter", Header -> True, 
+     RemoveZeros -> True}]] := 
   Module[{inversemetric, n, list}, n = Length[coord];
    inversemetric := Simplify[Inverse[metric]];
    Christoffel = 
@@ -203,7 +201,7 @@ ChristoffelSymbols[metric_, coord_,
            D[metric[[\[Mu], \[Beta]]], coord[[\[Nu]]]] -
            D[metric[[\[Mu], \[Nu]]], coord[[\[Beta]]]]), {\[Beta], 1, 
          n}],
-      {\[Alpha], 1, n}, {\[Mu], 1, n}, {\[Nu], 1, n}]];
+      {\[Nu], 1, n}, {\[Alpha], 1, n}, {\[Mu], 1, n}]];
    
    (* handling options *)
    fontSize = OptionValue[FontSize];
@@ -233,7 +231,6 @@ ChristoffelSymbols[metric_, coord_,
     TableSpacing -> {2, 1}, 
     If[SameQ[useHeader, True], TableHeadings -> {None, {a, "", b}}]]
    ];
-
 
 Affine[g_,xx_]:=Block[{n,ig,res},n= Length[xx];ig=InverseMetric[g];
 res=Table[(1/2)*Sum[ig[[i,s]]*(-D[g[[j,k]],xx[[s]]]+D[g[[j,s]],xx[[k]]]+D[g[[s,k]],xx[[j]]]),{s,1,n}],{i,1,n},{j,1,n},{k,1,n}];
